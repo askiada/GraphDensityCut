b4955a5d89aed0ac9734131c9d25ef35dc404263
Skiada Alexandre <alexandre@bokur.com>
2019-04-17 15:58:06 +0100

    - removed DCTcount (useless hashmap). - added a hashmap to store nodes similarity - added comments

--- src/main.go
+++ src/main.go
@@ -4,76 +4,86 @@ import (
 	"math"
 	"math/rand"
 
-	metric "github.com/askiada/Dcut/src/distance"
+	metric "github.com/askiada/GraphDensityCut/src/distance"
 
-	"github.com/askiada/Dcut/src/model"
+	"github.com/askiada/GraphDensityCut/src/model"
 )
 
 type Session struct {
-	DCTCount       map[int]int
 	DCTEdges       map[int][]*model.Edge
 	ExploreResults map[int]map[int]int
+	T              []*model.Node
 }
 
-func (s *Session) DensityConnectedTree(g []*model.Node, first *int) []*model.Node {
-
-	s.DCTCount = make(map[int]int)
+func (s *Session) DensityConnectedTree(g []*model.Node, first *int) {
+	//s.DCTCount = make(map[int]int)
 	s.DCTEdges = make(map[int][]*model.Edge)
 	gSize := len(g)
 	//T = null;
 	var T []*model.Node
-	//Set ∀v ∈ V as unchecked (v.checked = f alse); --> Zero value for boolean
+	//Set ∀v ∈ V as unchecked (v.checked = false); --> Zero value for boolean
 	//Randomly selected one node u ∈ V ;
 	if first == nil {
-		*first = rand.Intn(len(g))
+		tmp := rand.Intn(len(g))
+		first = &tmp
 	}
 	//Set u.checked = true;
 	g[*first].Checked = true
 
-	//u.connect = null, and u.density = null; --> Zero value for pointers
+	//u.connect = null, and u.density = null; --> Zero value for pointer connect. Density is equal to 0 by default, and it does not matter
 
 	//T.insert(u);
-
+	metric.Init(len(g))
 	T = append(T, g[*first])
 	for true {
+		//maxv = −1; p = null; q = null;
 		maxv := float64(-1)
 		var p, q *model.Node
-
 		//while T.size < V.size do
 		if len(T) >= gSize {
 			break
 		}
-
+		//for j = 1 to Γ(u).size do
 		for i := range T {
 			u := T[i]
 			for j := range u.Neighbors {
-				v := u.Neighbors[j]
-				if !g[v.To].Checked {
-					suv := metric.JaccardCoeff(u, g[v.To])
-					if suv > maxv {
-						maxv = suv
-						p = g[v.To]
+				//v = Γ(u).get(j);
+				vEdge := u.Neighbors[j]
+				v := g[vEdge.To]
+				//if v.checked == false then
+				if !v.Checked {
+					//If we have already computed the NodeSimilarity for an edge, we can use the score from the previous computation
+					if vEdge.NodeSimilarity == nil {
+						tmp := metric.NodeSim(u, v, vEdge.Weight)
+						vEdge.NodeSimilarity = &tmp
+					}
+					//if s(u, v) > maxv then
+					if *vEdge.NodeSimilarity > maxv {
+						maxv = *vEdge.NodeSimilarity
+						p = v
 						q = u
 					}
 				}
 			}
 		}
-
 		p.Checked = true
 		p.Connect = q
 		p.Density = maxv
-
-		s.DCTCount[p.Value]++
-		s.DCTCount[q.Value]++
+		//After each iteration, we create a new edge in the Density Connected Tree.
+		//Check is true, because we want to only check the Dcut bi-partition for one of the edge.
 		s.DCTEdges[p.Value] = append(s.DCTEdges[p.Value], &model.Edge{To: q.Value, Weight: maxv, Check: true})
 		s.DCTEdges[q.Value] = append(s.DCTEdges[q.Value], &model.Edge{To: p.Value, Weight: maxv})
+
+		//T.insert(p);
 		T = append(T, p)
 	}
-	return T
+	s.T = T
+	//return T
 }
 
 func (s *Session) explore(node int, exclude int) int {
 	val, ok := s.ExploreResults[node]
+
 	if !ok {
 		s.ExploreResults[node] = make(map[int]int)
 	} else {
@@ -81,8 +91,9 @@ func (s *Session) explore(node int, exclude int) int {
 			return storedScore
 		}
 	}
+
 	//-1 to exclude
-	count := s.DCTCount[node] - 1
+	count := len(s.DCTEdges[node]) - 1
 	for _, edge := range s.DCTEdges[node] {
 		if edge.To != exclude {
 			count += s.explore(edge.To, node)
@@ -100,19 +111,24 @@ func min(a, b int) int {
 }
 
 func (s *Session) Dcut() (int, int, float64) {
+	//For each nodeA we want to count the number of nodes in the partition if we break the edge between nodeA and nodeB
+	//map[nodeA][nodeB]partitionSize
 	s.ExploreResults = make(map[int]map[int]int)
 	minDcut := math.Inf(1)
 	minFrom := -1
 	minTo := -1
 
+	//For each edge in the Density Connected Tree, we evaluate the score of the two partitions defined after removing that edge
 	dcut := float64(0)
 	for node, edges := range s.DCTEdges {
 		for _, e := range edges {
 			if e.Check {
+				//Count partiion should also include the node itself --> + 1
 				countParition := s.explore(node, e.To) + 1
-				dcut = e.Weight / float64(min(countParition, len(s.DCTCount)-countParition))
+				//Dcut(C1, C2) = d(C1, C2)/min(|C1|, |C2|)
+				dcut = e.Weight / float64(min(countParition, len(s.T)-countParition))
 
-				if dcut < minDcut && dcut != 0 {
+				if dcut < minDcut {
 					minDcut = dcut
 					minFrom = node
 					minTo = e.To
@@ -124,5 +140,5 @@ func (s *Session) Dcut() (int, int, float64) {
 }
 
 func main() {
-
+	//Nothing for now
 }
